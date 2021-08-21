6c9b6feeb777c0cc1fe85bb0d31fea773c201af7
faks31 <tofaraz31@gmail.com>
2021-08-21 06:02:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Header';
+import Books from './Books';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header />
+      <Books />
     </div>
-  );
+    );
+  }
 }
 
-export default App;
